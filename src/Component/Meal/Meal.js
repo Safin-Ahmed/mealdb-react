32b58485f63d820bfe4905b2215222897cf1389a
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-09-25 02:25:23 +0600

    Added font awesome icons in the button

--- src/Component/Meal/Meal.js
+++ src/Component/Meal/Meal.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Meal.css';
 
 const Meal = (props) => {
     const {strArea, idMeal, strMeal, strMealThumb, strYoutube} = props.meal;
+    const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className = "single-meal">
             <img src={strMealThumb} alt="" />
@@ -10,7 +13,7 @@ const Meal = (props) => {
             <h3>Food Name : {strMeal}</h3>
             <h4>Made In: {strArea}</h4>
             <button><a href={strYoutube}>Watch Video</a></button>
-            <button onClick = {() => {props.addMeal(props.meal)}} className="add-meal">Add the Meal</button>
+            <button onClick = {() => {props.addMeal(props.meal)}} className="add-meal">{element} Add the Meal</button>
         </div>
     );
 };
