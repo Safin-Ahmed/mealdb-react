bfff907b1cec1d2862b36f8caaa34797d9c8355d
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-09-25 04:42:33 +0600

    Added the search functionality

--- src/Component/Meals/Meals.js
+++ src/Component/Meals/Meals.js
@@ -6,26 +6,44 @@ import './Meals.css';
 const Meals = () => {
     const [meals, setMeals] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayMeals, setDisplayMeals] = useState([]);
     useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
         .then(res => res.json())
-        .then(data => setMeals(data.meals));
+        .then(data =>{
+            setMeals(data.meals);
+            setDisplayMeals(data.meals);
+        })
     }, [])
+
     const handleAddMeal = product => {
         const newCart = [...cart, product];
         setCart(newCart);
     }
+    const handleSearch = event => {
+        const searchText = event.target.value;
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText ? `${searchText}`:`a`}`)
+        .then(res => res.json())
+        .then(data => setDisplayMeals(data.meals));
+    }
     return (
-        <div className = "meals">
-            <div className = "meals-container">
-                {
-                    meals.map(meal => <Meal addMeal = {handleAddMeal} key = {meal.idMeal} meal = {meal}></Meal>)      
-                }
-            </div>
-            <div className = "cart-container">
-                <h2>Order Summary</h2>
-                <Cart cart = {cart}></Cart>
-            </div>
+        <div>
+          <div>
+              <div className = "search-container">
+                <input onChange={handleSearch} placeholder="Search Your Tasty Food" type="text" />
+              </div>
+                <div className = "meals">
+                <div className = "meals-container">
+                    {
+                     displayMeals.map(meal => <Meal addMeal = {handleAddMeal} key = {meal.idMeal} meal = {meal}></Meal>)      
+                    }
+                </div>
+                <div className = "cart-container">
+                    <h2>Order Summary</h2>
+                    <Cart cart = {cart}></Cart>
+                </div>
+        </div>
+          </div>
         </div>
     );
 };
